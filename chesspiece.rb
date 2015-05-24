441120564544a6187f52807eb71fdd953899b857
Rory Ippel <rjippel@gmail.com>
2015-05-24 12:13:16 -0500

    path_blocked added, tests for horizontal and diagonal movement passing

--- chesspiece.rb
+++ chesspiece.rb
@@ -1,7 +1,12 @@
 class ChessPiece
-  def initialize(board, square)
+  attr_reader :location, :board
+
+  def initialize(board, square, add_to_board = true)
     @location = square
     @board = board
+    if add_to_board
+      board.add_piece(self, square)
+    end
   end
 
   def move_vertical(square)
@@ -12,11 +17,30 @@ class ChessPiece
 
     step = move_length > 0 ? 1 : -1
     (0...move_length).each do |i|
-      File.open("./vars.log", "a") { |io| io.puts "i: #{i} move_length#{move_length}" + ([(@location[0] +  (i + 1)*step), @location[1]]).to_s  }
       if @board.position_occupied?([(@location[0] +  (i + 1)*step), @location[1]]) then return false end
     end
 
     @board.move_piece  @location, square
+  end
+
+  def path_clear?(end_point)
+    move_length_row = @location[0] - end_point[0]
+    move_length_col = @location[1] - end_point[1]
+
+    step_row = move_length_row == 0 ? 0 : (move_length_row / move_length_row)
+    step_col = move_length_col == 0 ? 0 : (move_length_col / move_length_col)
+
+    #greater of the row and col lengths; chance sign to positive for iteration
+    length = move_length_row.abs > move_length_col.abs ? move_length_row : move_length_col
+    length = length >= 0 ? length : length * -1
+
+    (1...length).each do |i|
+      File.open("./vars.log", "a") { |io| io.puts "step_row: #{step_row}, step_col: #{step_col}, length: #{length}"  }
+      if board.position_occupied? ([@location[0] + (i * step_row), @location[1] + (i * step_col)])
+        return false
+      end
+    end
+    true
   end
 
 end
