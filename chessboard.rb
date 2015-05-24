1b02b8d6fdb3131a4e6d5866c815dad244f83fc0
Rory Ippel <rjippel@gmail.com>
2015-05-23 19:16:32 -0500

    ChessPiece#move_vertical added

--- chessboard.rb
+++ chessboard.rb
@@ -13,11 +13,14 @@ class ChessBoard
   	square[0].between(0,7) && square[1].between(0,7)
   end
 
-  def move_piece(square)
+  def move_piece(from, destination)
     if position_occupied?(square, chesspiece)
       return false
     else
-      @positions[square[0]][square[1]] = chesspiece
+      @positions[destination[0]][destination[1]] = @positions[from[0]][from[1]]
+      @positions[from[0]][from[1]] = nil
     end
+    square
   end
+
 end
--- chesspiece.rb
+++ chesspiece.rb
@@ -1,5 +1,21 @@
 class ChessPiece
-  def initialize(square)
+  def initialize(board, square)
     @location = square
   end
+
+  def move_vertical(square)
+    if board.inbounds? square && board.position_occupied? && square[1] == location[1]
+      move_length = location - square
+    else
+      return false
+    end
+
+    step = move_length > 0 ? 1 : -1
+    move_length.step(step) do
+      if board.position_occupied? then return false end
+    end
+
+    board.move_piece square
+  end
+
 end
