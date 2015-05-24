4bfd4eeb45889226f5d58b3ea44e7b68e24254e3
Rory Ippel <rjippel@gmail.com>
2015-05-24 12:29:18 -0500

    Refactored path_clear?

--- chesspiece.rb
+++ chesspiece.rb
@@ -27,20 +27,28 @@ class ChessPiece
     move_length_row = @location[0] - end_point[0]
     move_length_col = @location[1] - end_point[1]
 
-    step_row = move_length_row == 0 ? 0 : (move_length_row / move_length_row)
-    step_col = move_length_col == 0 ? 0 : (move_length_col / move_length_col)
+    step_row = get_step move_length_row 
+    step_col = get_step move_length_col 
 
-    #greater of the row and col lengths; chance sign to positive for iteration
-    length = move_length_row.abs > move_length_col.abs ? move_length_row : move_length_col
-    length = length >= 0 ? length : length * -1
+    length = greater_difference(move_length_col, move_length_row)
 
     (1...length).each do |i|
-      File.open("./vars.log", "a") { |io| io.puts "step_row: #{step_row}, step_col: #{step_col}, length: #{length}"  }
       if board.position_occupied? ([@location[0] + (i * step_row), @location[1] + (i * step_col)])
         return false
       end
     end
     true
+  end
+
+  private
+
+  def greater_difference(length_one, length_two)
+    length = length_one.abs > length_two.abs ?  length_one : length_two
+    length.abs
+  end
+
+  def get_step (size)
+    size == 0 ? 0 : (size / size)
   end
 
 end
